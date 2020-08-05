9df0d6907df8412cf3cba23b17aee56bfcfa5fa3
Michael Du Toit <michael.dtoit@gmail.com>
2020-08-05 15:33:12 +1200

    Added hero jump animation along with script (except force).

---
+++ Assets/HeroScript.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using UnityEngine;
+
+
+public class HeroScript : MonoBehaviour
+{
+    public float jumpForce = 7.0f;
+    private bool onGround = false;
+    public int movementSpeed = 7;
+    private Animator anim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("up"))
+        {
+            if(onGround == true)
+            {
+                Rigidbody2D rb = GetComponent<Rigidbody2D>();
+                //rb.AddForce(new Vector2(0f, jumpForce), ForceMode2D.Impluse); <-- needs fixing
+                onGround = false;
+                anim.SetTrigger("Jump");
+            }
+        }
+        
+    }
+
+    void OnCollisionEnter2D(Collision2D hit)
+    {
+        onGround = true;
+        print("Hero has collided with ground");
+    }
+
+}
