64007258709abb87bda5281d9fc9d1fda43907a2
Michael Du Toit <michael.dtoit@gmail.com>
2020-08-12 15:24:14 +1200

    Added camera tracking to heroFollower.

--- Assets/heroFollower.cs
+++ Assets/heroFollower.cs
@@ -1,9 +1,11 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Security.Cryptography;
 using UnityEngine;
 
 public class heroFollower : MonoBehaviour
 {
+    public GameObject heroCopy;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +15,9 @@ public class heroFollower : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(heroCopy.transform.position.x > transform.position.x)
+        {
+            transform.position = new Vector2(heroCopy.transform.position.x, -2.05f);
+        }
     }
 }
