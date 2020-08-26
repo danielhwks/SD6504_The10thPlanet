0a1d85a13f7cbf6e539b2329da02a77ae979dd61
GoogzGazuli <danielhwks9@gmail.com>
2020-08-26 16:11:26 +1200

    RFC Might have fixed camera issue

--- Assets/Scripts/HeroFollower.cs
+++ Assets/Scripts/HeroFollower.cs
@@ -9,7 +9,21 @@ public class HeroFollower : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        /*
+        Debug.Log(Screen.width/100.0);
+        Debug.Log((Screen.width/100.0f)/2);
+        float screenHalfWidth = Camera.main.orthographicSize * Screen.width / Screen.height;
+        Debug.Log(screenHalfWidth);
+        Debug.Log(Camera.main.orthographicSize);
+        GameObject camera = GameObject.Find("Main Camera");
+        camera.transform.localPosition = new Vector3(15f - screenHalfWidth, camera.transform.localPosition.y, camera.transform.localPosition.z);
+        */
+        float screenHalfWidth = Camera.main.orthographicSize * Screen.width / Screen.height;
+        //float edge = screenHalfWidth - 2.65f;
+        float edge = 12.36f;
+        float offset = screenHalfWidth - edge;
+        GameObject camera = GameObject.Find("Main Camera");
+        camera.transform.localPosition = new Vector3(offset, camera.transform.localPosition.y, camera.transform.localPosition.z);
     }
 
     // Update is called once per frame
