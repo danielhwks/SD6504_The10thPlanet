3b364f0542aabbb18a29dc45e4f3c2bc7ef5ba79
Michael Du Toit <michael.dtoit@gmail.com>
2020-08-28 10:58:47 +1200

    Added Help Scene. Still work in progress.

--- Assets/Scripts/HelpScript.cs
+++ Assets/Scripts/HelpScript.cs
@@ -4,24 +4,17 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
-public class ChangeScenes : MonoBehaviour
+public class HelpScript : MonoBehaviour
 {
-    public Button StartBtn;
     public Button HelpBtn;
     private AssetBundle assetBundle;
     // Start is called before the first frame update
     void Start()
     {
-        StartBtn.onClick.AddListener(() =>
-        {
-            SceneManager.LoadScene("GameScene");
-        });
-
         HelpBtn.onClick.AddListener(() =>
         {
-            SceneManager.LoadScene("HelpScene");
+            SceneManager.LoadScene("HomeScene");
         });
-
     }
 
     // Update is called once per frame
