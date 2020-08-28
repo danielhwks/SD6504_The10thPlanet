f9152d02c4addf16ea0f1966866eb7556c566660
GoogzGazuli <danielhwks9@gmail.com>
2020-08-28 13:34:58 +1200

    Fixed help menu

--- Assets/Scripts/HelpScript.cs
+++ Assets/Scripts/HelpScript.cs
@@ -7,13 +7,12 @@ using UnityEngine.SceneManagement;
 public class HelpScript : MonoBehaviour
 {
     public Button BackBtn;
-    private AssetBundle assetBundle;
     // Start is called before the first frame update
     void Start()
     {
         BackBtn.onClick.AddListener(() =>
         {
-            SceneManager.LoadScene("HelpScene");
+            SceneManager.LoadScene("HomeScene");
         });
     }
 
