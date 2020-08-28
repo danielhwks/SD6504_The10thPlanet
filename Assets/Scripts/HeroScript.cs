decfc0a32daf5b829358adf18cf1aa0a8815d7f2
GoogzGazuli <danielhwks9@gmail.com>
2020-08-28 13:51:58 +1200

    Basic scoring function

--- Assets/Scripts/HeroScript.cs
+++ Assets/Scripts/HeroScript.cs
@@ -96,4 +96,8 @@ public class HeroScript : MonoBehaviour
         print("Hero Health: " + heroHealth);
     }
 
+    public void GetScore()
+    {
+        return (int)transform.position.x;
+    }
 }
