10647f1ebeb0c3bb02b2c0cace1beb053e94457c
Michael Du Toit <michael.dtoit@gmail.com>
2020-08-30 20:05:35 +1200

    UI Info is now fixed to the top-left corner of the screen and moves with camera.

---
+++ Assets/Scripts/UIInfoScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using UnityEngine;
+
+public class UIInfoScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    //The positioning on the UI Script must be in FixedUpdate to avoid jittering effect when the camera moves.
+    void FixedUpdate()
+    {
+        float cameraHalfHeight = Camera.main.orthographicSize;
+        float cameraHalfWidth = Camera.main.aspect * cameraHalfHeight;
+        Bounds bounds = GetComponent<Renderer>().bounds;
+        Vector3 topLeftPos = new Vector3(-cameraHalfWidth, cameraHalfHeight, 0) + Camera.main.transform.position;
+        topLeftPos += new Vector3(bounds.size.x / 2, -bounds.size.y / 2, 1);
+        transform.position = topLeftPos;
+    }
+}
